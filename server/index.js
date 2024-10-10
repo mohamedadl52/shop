12ffca7ec7d0b7cb0358cf304b59e60f5d8af1b7
mohamedadl52 <65096134+mohamedadl52@users.noreply.github.com>
2024-10-11 00:23:31 +0200

    تعديل بسيط اخر

--- server/index.js
+++ server/index.js
@@ -96,7 +96,7 @@ function initial() {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'));
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html');
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html')) ;
 }
 
 const PORT = process.env.PORT || 8081;
